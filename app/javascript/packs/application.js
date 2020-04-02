837c0d7016561f09ebe2a3f1f7652ffc12fa9133
Daniel Gaytan <dgaytan6@gmail.com>
2020-04-02 14:05:55 -0600

    Initial landing page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,23 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import "bootstrap/dist/js/bootstrap"
+import '../stylesheets/application'
+
+$(function(){
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    let target = this.hash;
+    let $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop':  $target.offset().top //no need of parseInt here
+    }, 900, 'swing', function () {
+        window.location.hash = target;
+    });
+  });
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
