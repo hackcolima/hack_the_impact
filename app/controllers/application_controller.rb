790bd79d4a922e2737ba652fb355685bdb4fbf57
Daniel Gaytan <dgaytan6@gmail.com>
2020-04-02 20:05:54 -0600

    Allow production to redirect to https, and also redirect to www.hackcolima.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :add_www_subdomain, if: proc { Rails.env.production? }
+
+  private
+
+  def add_www_subdomain
+    return if /^www/.match(request.host)
+
+    redirect_to("#{request.protocol}www.hackcolima.com#{request.fullpath}", status: 301)
+  end
 end
