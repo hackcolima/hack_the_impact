af37681ae04c0c949a64e9cb82ac02dc41c08311
Daniel Gaytan <dgaytan6@gmail.com>
2020-04-03 20:09:43 -0600

    Change registration url and remove partner join button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def registration_form_url
-    "https://docs.google.com/forms/d/e/1FAIpQLScuKWSqpf75JLr0-mqFvybO50Lsr19uCSxs8YnBxx9Y5wiQgQ/viewform?usp=sf_link"
+    "https://airtable.com/shrdJhPAVSK5ts0fL"
   end
 end
