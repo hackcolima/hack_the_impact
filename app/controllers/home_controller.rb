384df737b9a77cf03f6f3cf3ecc94c459caae23d
Daniel Gaytan <dgaytan6@gmail.com>
2020-04-06 20:18:09 -0500

    Introduce challenges

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
+  layout :resolve_layout
+
   def index
+  end
+
+  def challenges
+  end
+
+  private
+
+  def resolve_layout
+    case action_name
+    when "index"
+      "landing"
+    else
+      "application"
+    end
   end
 end
